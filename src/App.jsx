ec754482e656020ae852dfae92487b0dc75f3a88
mocilnikcarlos <mocilnik.web@gmail.com>
2022-06-18 14:00:27 -0300

    fix(Root): Fix element in route

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { Fragment } from "react";
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/common/header/Header";
-import IndexHero from "./pages/home/IndexHero";
+import IndexHome from "./pages/home/IndexHome";
 
 const App = () => {
   return (
     <Fragment>
       <Header />
       <Routes>
-        <Route path="/" element={<IndexHero />} />
+        <Route path="/" element={<IndexHome />} />
       </Routes>
     </Fragment>
   );
