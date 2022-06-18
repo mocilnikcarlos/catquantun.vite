3f5f1a6915fafd81f7fd45464134e2f01cd72235
mocilnikcarlos <mocilnik.web@gmail.com>
2022-06-18 11:23:30 -0300

    feat(Root): Added file hero

--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -1,9 +1,23 @@
+import MyName from "../../components/other/MyName";
+import { Location, Hability } from "../../components/other/InfoHero";
 import "./Hero.scss";
 
 const Hero = () => {
   return (
     <section className="box-section background-hero">
-      <article className="box-article article-hero">sfasf</article>
+      <article className="box-article article-hero">
+        <MyName placeholder="Yo soy Carlos Alberto Mocilnik" />
+        <div className="boxs-infohero">
+          <Location
+            placeholderup="Argentina"
+            placeholderdown="Zarate - Buenos Aires"
+          />
+          <Hability
+            placeholderup="Freelancer"
+            placeholderdown="Designer and Dev. Front-End"
+          />
+        </div>
+      </article>
     </section>
   );
 };
