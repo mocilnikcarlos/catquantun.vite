3236e06d8d5b82d27cb34bbcd6dc275bcd751067
mocilnikcarlos <mocilnik.web@gmail.com>
2022-06-18 11:22:42 -0300

    feat(Root): Added element poligons to hero

---
+++ src/components/fig/Poligons.jsx
@@ -0,0 +1,11 @@
+import "./Poligons.scss";
+
+const RowHorizontal = () => {
+  return <div className="row-horizontal"></div>;
+};
+
+const ColumnVertical = () => {
+  return <div className="column-vertical"></div>;
+};
+
+export { RowHorizontal, ColumnVertical };
