557097bf5814a132192a6b04095c35e6cd33580c
mocilnikcarlos <mocilnik.web@gmail.com>
2022-06-18 09:30:10 -0300

    feat(Root): Added hero

---
+++ src/pages/home/Hero.jsx
@@ -0,0 +1,11 @@
+import "./Hero.scss";
+
+const Hero = () => {
+  return (
+    <section className="box-section background-hero">
+      <article className="box-article article-hero">sfasf</article>
+    </section>
+  );
+};
+
+export default Hero;
