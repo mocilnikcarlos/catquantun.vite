ff6cb940455896bde5c1fe1e751583ef0c0889ec
mocilnikcarlos <mocilnik.web@gmail.com>
2022-06-18 09:20:07 -0300

    feat(Root): Added route file app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { Fragment } from "react";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/common/header/Header";
+import IndexHero from "./pages/home/IndexHero";
 
 const App = () => {
   return (
     <Fragment>
       <Header />
+      <Routes>
+        <Route path="/" element={<IndexHero />} />
+      </Routes>
     </Fragment>
   );
 };
