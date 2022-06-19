56e8566ae15b6a4586fba5c519713e6bb632a546
mocilnikcarlos <mocilnik.web@gmail.com>
2022-06-19 15:43:05 -0300

    chore(Root): Added text in section

--- src/pages/home/WorkDesigner.jsx
+++ src/pages/home/WorkDesigner.jsx
@@ -11,6 +11,15 @@ const WorkDesigner = () => {
     <section className="box-section section-work-designer">
       <article className="box-article article-work-designer">
         <TitleWeb2 placeholder="El trabajo de un Designer UX" />
+        <p className="textweb">
+          En pocas palabras, un diseñador UX está encargado de lograr generar la
+          mejor experiencia para el usuario. Tan simple y tan complejo a la vez,
+          se necesitan de muchas herramientas, desde psicológicas hasta
+          tecnológicas. Aun así, no solo es pensar en el usuario. Tener bien en
+          claro cuál es el objetivo de la empresa también entra en estos
+          requisitos, ya que será este el propósito que se verá reflejado en el
+          usuario.
+        </p>
         <div className="cards-work-designer">
           <CardCircle
             element={<UserSpeak />}
@@ -33,6 +42,14 @@ const WorkDesigner = () => {
             placeholderout="Testing"
           />
         </div>
+        <p className="textweb">
+          Para entrar en detalles de como se logra una buena experiencia de
+          usuario primero hay que entender que este proceso es un bucle y que no
+          acaba una vez obtenido un resultado. La sociedad avanza y con ella las
+          necesidades. Quedarse con un solo resultado implica estancarse en un
+          grupo reducido de personas, perdiendo así una gran cantidad de
+          tráfico.
+        </p>
       </article>
     </section>
   );
