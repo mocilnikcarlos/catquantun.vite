3a7ed8ef36d69580a62d1270df0fcc6bc6809eda
mocilnikcarlos <mocilnik.web@gmail.com>
2022-06-20 10:49:37 -0300

    feat(Root): Create and added GettinBetterUX

---
+++ src/pages/home/GettingBetterUx.jsx
@@ -0,0 +1,11 @@
+import "./GettingBetterUx.scss";
+
+const GettingBetterUx = () => {
+  return (
+    <section className="box-section section-better-ux">
+      <article className="box-article article-better-ux"></article>
+    </section>
+  );
+};
+
+export default GettingBetterUx;
