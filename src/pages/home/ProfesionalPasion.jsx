a46989851cad1266cf1a0b18d6a3dec5b5338991
mocilnikcarlos <mocilnik.web@gmail.com>
2022-06-25 10:25:56 -0300

    feat(Root): Create section professional pasion and added in indexhome

---
+++ src/pages/home/ProfesionalPasion.jsx
@@ -0,0 +1,14 @@
+import CardDesigner from "../../components/cards/CardDesigner";
+import "./ProfesionalPasion.scss";
+
+const ProfesionalPasion = () => {
+  return (
+    <section className="box-section profesional-pasion-section">
+      <article className="box-article profesional-pasion-article">
+        <CardDesigner />
+      </article>
+    </section>
+  );
+};
+
+export default ProfesionalPasion;
